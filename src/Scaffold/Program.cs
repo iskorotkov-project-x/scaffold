70b2bb75f8e6ee9309321f9390b62ae0b697014e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-05-13 16:08:50 +0500

    chore: подготовлена структура проекта и основные правила разработки

---
+++ src/Scaffold/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Scaffold
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            var services = new ServiceCollection();
+            
+            // Add services.
+            // services.AddScoped<InterfaceType, RealType>();
+            
+            using var serviceProvider = services.BuildServiceProvider();
+            
+            // Get required services.
+            // var v = serviceProvider.GetRequiredService<InterfaceType>();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
