35623ed7c2ff6ae9abea446a4efad7a12264cdf4
Дмитрий Ковыляев <354981@mail.ru>
2021-05-23 13:42:05 +0500

    refactor: add comments

--- tests/Loader.FileSystem.Tests/FileSystemLoaderTests.cs
+++ tests/Loader.FileSystem.Tests/FileSystemLoaderTests.cs
@@ -9,15 +9,19 @@ namespace Loader.Tests
         [Fact]
         public void LoadTemplate()
         {
+            // Arrange
             var loader = new FileSystemLoader($"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\.scaffold\\templates");
+            
+            // Act
             var template = loader.Load("c#", "tl1");
-
+            
+            // Assert
             Assert.NotNull(template);
             Assert.Equal("c#", template.Language);
             Assert.Equal("tl1", template.Name);
             Assert.NotEmpty(template.Files);
 
-            // in curent state we not use plugins
+            // in current state we not use plugins
             //Assert.NotEmpty(template.Plugins);
         }
     }
